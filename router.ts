e61d9f3ea21da8241d543cec211ad4aee9b30e77
FukeKazki <kazkichi0906@gmail.com>
2020-05-22 02:06:28 +0900

    Fix: 細い修正

--- router.ts
+++ router.ts
@@ -3,10 +3,10 @@ import {getBooks, getBook, addBook, updateBook, deleteBook} from './controller.t
 
 const router = new Router()
 router
-    .get('/books', getBooks)
-    .get('/books/:isbn', getBook)
-    .post('/books', addBook)
-    .put('/books/:isbn', updateBook)
-    .delete('/books/:isbn', deleteBook)
+	.get('/books', getBooks)
+	.get('/books/:isbn', getBook)
+	.post('/books', addBook)
+	.put('/books/:isbn', updateBook)
+	.delete('/books/:isbn', deleteBook)
 
 export default router
